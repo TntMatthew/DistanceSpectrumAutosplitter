7f986561e7b73d7d6d02de5dad3dc12e84842558
Matthew Tavendale <TntMatthew@users.noreply.github.com>
2018-03-05 22:05:51 +0000

    Initial commit

---
+++ DistanceAutosplitter/Plugin.cs
@@ -0,0 +1,80 @@
+﻿//using Spectrum;
+using Spectrum.API;
+using Spectrum.Interop.Game;
+using Spectrum.Interop.Game.Vehicle;
+using Spectrum.API.Interfaces.Plugins;
+using Spectrum.API.Interfaces.Systems;
+using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DistanceAutosplitter
+{
+    public class Entry : IPlugin, IUpdatable
+    {
+        public string FriendlyName => "Distance Autosplitter";
+        public string Author => "Matthew Tavendale";
+        public string Contact => "@TntMatthew#3201 on Discord";
+        public APILevel CompatibleAPILevel => APILevel.XRay;
+
+        TimeSpan totalElapsedTime = new TimeSpan();
+        bool countingTime = false;
+
+        Socket livesplitSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        public void Initialize(IManager manager)
+        {
+            livesplitSocket.Connect("localhost", 16834);
+            livesplitSocket.Send(Encoding.UTF8.GetBytes($"initgametime\r\n"));
+
+            LocalVehicle.Finished += (sender, args) =>
+            {
+                if (Game.CurrentMode == Spectrum.Interop.Game.GameMode.Adventure)
+                {
+                    totalElapsedTime += Race.ElapsedTime;
+                    livesplitSocket.Send(Encoding.UTF8.GetBytes($"setgametime {totalElapsedTime.TotalSeconds}\r\n"));
+                    livesplitSocket.Send(Encoding.UTF8.GetBytes("split\r\n"));
+                    livesplitSocket.Send(Encoding.UTF8.GetBytes("pausegametime\r\n"));
+                    countingTime = false;
+                    if (Game.LevelName == "Credits")
+                    {
+                        totalElapsedTime = new TimeSpan();
+                    }
+                }
+            };
+
+            Race.Started += (sender, args) =>
+            {
+                if (Game.CurrentMode == Spectrum.Interop.Game.GameMode.Adventure)
+                {
+                    Console.WriteLine("started");
+                    Console.WriteLine(Game.LevelName);
+                    if (Game.LevelName == "Broken Symmetry")
+                    {
+                        livesplitSocket.Send(Encoding.UTF8.GetBytes("starttimer\r\n"));
+                    }
+                    else
+                    {
+                        livesplitSocket.Send(Encoding.UTF8.GetBytes("unpausegametime\r\n"));
+                    }
+                }
+                countingTime = true;
+            };
+
+        }
+
+        public void Update()
+        {
+            if (countingTime)
+            {
+                TimeSpan countingTime = totalElapsedTime + Race.ElapsedTime;
+                livesplitSocket.Send(Encoding.UTF8.GetBytes($"setgametime {countingTime.TotalSeconds}\r\n"));
+            }
+        }
+
+        public void Shutdown()
+        {
+
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,30 @@
+# Distance Spectrum Autosplitter
+This is a Spectrum plugin that provides automatic splits and an IGT timer for Distance speedruns through
+LiveSplit Server.
+
+This is very early in development. I haven't actually used this in a full run yet, so I don't know if
+everything works properly (it should). There's no option to disable it yet, so until I get that in, I am
+currently locking it to only work in Adventure mode. I want to have it automatically reset if you return to
+the main menu, but I haven't figured out how to detect that yet, so you'll have to manually reset for now.
+
+**Note**: It is not yet decided whether or not using Spectrum will be allowed for runs, so this might be useless.
+We'll see. 
+
+## How to use
+First, install the LiveSplit Server component: https://github.com/LiveSplit/LiveSplit.Server/releases
+
+After you've done that, you need to add the LiveSplit Server component to your layout. Then right-click on
+LiveSplit, go into the Control submenu, and select Start Sever. Note that you must do this every time you
+start LiveSplit, and you should do this before you open the game. The plugin connects to the server as
+soon as it's loaded, so if you start the server after you open the game, it will silently fail (there's no
+debugging info in -console yet).
+
+Note: If you want to use RTA timing, you will have to start the timer manually; do not use the automatic
+start for timing runs in real time, it is not accurate to the timing method actually used. You won't have
+to worry about anything if you use IGT, though, apart from the fact there is no game time field on the sr.c
+boards yet. So you probably won't want to actually use that part of the autosplitter yet.
+
+You then need to install Distance Autosplitter plugin. Grab the latest .dll from the
+[releases page](https://github.com/TntMatthew/DistanceAutosplitter/releases), and drop
+it in your `Distance_Data/Spectrum/Plugins` directory. And you're done - the timer will
+automatically start once IGT begins, and will split after each level is completed.
